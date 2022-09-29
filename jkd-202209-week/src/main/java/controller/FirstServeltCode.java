9125b7e9a23d34c6c5824c9ddd334128b62a128f
yuyu <1220691996@qq.com>
2022-09-29 11:26:09 +0800

    homework1

--- jkd-202209-week/src/main/java/controller/FirstServeltCode.java
+++ jkd-202209-week/src/main/java/controller/FirstServeltCode.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javax.servlet.*;
+import java.io.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -19,22 +20,21 @@ public class FirstServeltCode implements Servlet {
 
     @Override
     public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
-        //1. 设置文档的类型
+//        //1. 设置文档的类型
         resp.setContentType("text/html;charset=utf-8");
-        //2. 获取文档输出流
+//        //2. 获取文档输出流
         PrintWriter out = resp.getWriter();
-        //3. 直接输出内容到网页
+//        //3. 直接输出内容到网页
         //前端代码和java后端代码耦合在一块儿了.
-        out.println("<!doctype html>");//h5的文档的头的声明部分
-        out.println("<html>");
-        out.println("   <head>");
-        out.println("       <title>a港大</title>");
-        out.println("       <meta charset=utf-8>");
-        out.println("   </head>");
-        out.println("   <body>");
-        out.println("        <h3>第一个servlet程序</h3>");
-        out.println("   </body>");
-        out.println("</html>");
+        FileInputStream ip = new FileInputStream("src/main/webapp/login.html");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(ip));
+        String str = null;
+        while ((str = rd.readLine()) != null)
+        {
+            out.println(str);
+        }
+        ip.close();
+        rd.close();
     }
 
     @Override
